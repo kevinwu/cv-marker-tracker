7dbcc75ddb8524cd9adc1482c72956b1399764e9
Kevin Wu <kevinwu@bellplatform.com>
2016-06-22 23:32:37 +0200

    First commit

---
+++ build/Sheet3.build/Debug/Sheet3.build/Objects-normal/x86_64/PoseEstimation.d
@@ -0,0 +1,33 @@
+dependencies: \
+  /Users/kevinwu/Documents/TUM/4.Semester/Augmented\ Reality/Projects/Sheet3/PoseEstimation.cpp \
+  /usr/local/include/opencv/cv.h \
+  /usr/local/include/opencv2/core/core_c.h \
+  /usr/local/include/opencv2/core/types_c.h \
+  /usr/local/include/opencv2/core/cvdef.h \
+  /usr/local/include/opencv2/core/hal/interface.h \
+  /usr/local/include/opencv2/core.hpp \
+  /usr/local/include/opencv2/core/version.hpp \
+  /usr/local/include/opencv2/core/base.hpp \
+  /usr/local/include/opencv2/core/cvstd.hpp \
+  /usr/local/include/opencv2/core/ptr.inl.hpp \
+  /usr/local/include/opencv2/core/neon_utils.hpp \
+  /usr/local/include/opencv2/core/traits.hpp \
+  /usr/local/include/opencv2/core/matx.hpp \
+  /usr/local/include/opencv2/core/saturate.hpp \
+  /usr/local/include/opencv2/core/fast_math.hpp \
+  /usr/local/include/opencv2/core/types.hpp \
+  /usr/local/include/opencv2/core/mat.hpp \
+  /usr/local/include/opencv2/core/bufferpool.hpp \
+  /usr/local/include/opencv2/core/mat.inl.hpp \
+  /usr/local/include/opencv2/core/persistence.hpp \
+  /usr/local/include/opencv2/core/operations.hpp \
+  /usr/local/include/opencv2/core/cvstd.inl.hpp \
+  /usr/local/include/opencv2/core/utility.hpp \
+  /usr/local/include/opencv2/core/optim.hpp \
+  /usr/local/include/opencv2/imgproc/imgproc_c.h \
+  /usr/local/include/opencv2/imgproc/types_c.h \
+  /usr/local/include/opencv2/photo/photo_c.h \
+  /usr/local/include/opencv2/video/tracking_c.h \
+  /usr/local/include/opencv2/objdetect/objdetect_c.h \
+  /usr/local/include/opencv/cxcore.h \
+  /Users/kevinwu/Documents/TUM/4.Semester/Augmented\ Reality/Projects/Sheet3/PoseEstimation.h
